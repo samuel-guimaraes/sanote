4f500d976e6f87899c7f46afd18de5fb8bee6c65
samuel-guimares <sam.gustavo.guimaraes@gmail.com>
2024-08-01 02:46:17 -0300

    Criado o painel principal (commit inicial)
      - Criado o painel principal da aplicação.

---
+++ src/main/java/com/samg/sanote/gui/Main.java
@@ -0,0 +1,28 @@
+package com.samg.sanote.gui;
+
+import javax.swing.JPanel;
+
+/**
+ * Painel principal da aplicação
+ */
+public class Main extends JPanel {
+
+    /**
+     * Inicialização gráfica e configuração dos componentes.
+     */
+    public Main() {
+        initComponents();
+    }
+
+    @SuppressWarnings("unchecked")
+    // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
+    private void initComponents() {
+
+        setPreferredSize(new java.awt.Dimension(328, 540));
+        setLayout(new java.awt.BorderLayout());
+    }// </editor-fold>//GEN-END:initComponents
+
+
+    // Variables declaration - do not modify//GEN-BEGIN:variables
+    // End of variables declaration//GEN-END:variables
+}
